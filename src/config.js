8d97b125e3362a53a0298b44e76a0ddae3caea0d
Kevin Lazich <dev@kevinlazich.com>
2018-06-05 13:07:53 -0400

    chore: Continue testing

--- src/config.js
+++ src/config.js
@@ -2,15 +2,32 @@
 
 import { URL } from 'url'
 
+// louisvilleky.gov - News
 export const FEED_NEWS = new URL(
   'https://louisvilleky.gov/government/all/news/feed'
 )
+
+// louisvilleky.gov - Events
 export const FEED_EVENTS = new URL(
   'https://louisvilleky.gov/government/all/events/feed'
 )
+
+// Louisville Emergency Network - Alerts
 export const FEED_LEN_ALERT = new URL(
   'http://content.getrave.com/rss/kymsjefferson/channel1'
 )
+
+// Louisville Emergency Network - Notifications
 export const FEED_LEN_NOTIFICATION = new URL(
   'http://content.getrave.com/rss/kymsjefferson/channel2'
+)
+
+// Air Quality API
+export const JSON_AIR_QUALITY = new URL(
+  'https://aaws.louisvilleky.gov/api/v1/Monitor/CityAQI'
+)
+
+// Waze Traffic Feed
+export const JSON_WAZE_TRAFFIC = new URL(
+  'https://www.waze.com/rtserver/broadcast/BroadcastRSS?bid=86&format=JSON'
 )
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 // @flow
 
 import Parser from 'rss-parser'
+import request from 'request'
+const rp = require('request-promise')
 
 import * as config from './config.js'
 
 const parser = new Parser()
-// const handleError = (e: Error): void => console.error(e)
-
-// rss-parser scratchings
-
-// parser.parseURL(config.FEED_NEWS.href, function(err, feed) {
-//   err ? console.error(err) : console.dir(Object.keys(feed.items[0]))
-// })
 
 const dropKeys = (object: Object, ...keys: string[]): dropKeys | Object => {
   const [key, ...remaining] = keys
@@ -19,11 +14,47 @@ const dropKeys = (object: Object, ...keys: string[]): dropKeys | Object => {
   return remaining.length > 0 ? dropKeys(otherKeys, ...remaining) : otherKeys
 }
 
-parser
-  .parseURL(config.FEED_NEWS.href)
-  .then(feed => feed.items)
-  .then(items =>
-    items.map(item => dropKeys(item, 'dc:creator', 'contentSnippet'))
-  )
-  .then(items => items.forEach(item => console.log(item)))
-  .catch((err: Error) => console.error(err))
+// parser
+//   .parseString(config.JSON_WAZE_TRAFFIC.href)
+//   .then(feed => console.log(feed))
+//   // .then(keys => console.log(keys))
+//   // .then(items =>
+//   //   items.map(item => dropKeys(item, 'dc:creator', 'contentSnippet'))
+//   // )
+//   // .then(items => items.forEach(item => console.log(item)))
+//   .catch((err: Error) => console.error(err))
+
+request(
+  {
+    url: config.JSON_WAZE_TRAFFIC.href,
+    json: true,
+    headers: {
+      Connection: 'keep-alive',
+    },
+  },
+  (error, response, body) => {
+    error
+      ? console.error(error)
+      : response.statusCode !== 200
+        ? console.log('Wrong status code')
+        : console.log(body)
+  }
+)
+
+// request
+//   .get(config.JSON_AIR_QUALITY.href)
+//   .on('error', error => console.error(error))
+//   .on('response', res => {
+//     console.log(res)
+//   })
+//   .end()
+
+// const options = {
+//   uri: config.JSON_AIR_QUALITY.href,
+//   headers: { 'User-Agent': 'Request-Promise' },
+//   json: true,
+// }
+
+// rp(options)
+//   .then(resp => console.dir(resp))
+//   .catch(error => console.error(error))
